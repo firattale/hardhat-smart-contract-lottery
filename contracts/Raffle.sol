c27651a06235433619e07fb3a2ae82fd39b12778
Firat Tale <firat.tale@auto1.com>
2022-07-22 10:05:43 +0200

    add vrf consumer contract

--- contracts/Raffle.sol
+++ contracts/Raffle.sol
@@ -1,15 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+
 error Raffle__NotEnoughEthEntered();
 
-contract Raffle {
+contract Raffle is VRFConsumerBaseV2 {
     uint256 private immutable i_entranceFee;
     address payable[] private s_players;
 
     event RaffleEnter(address indexed player);
 
-    constructor(uint256 _entranceFee) {
+    constructor(address vrfCoordinator, uint256 _entranceFee)
+        VRFConsumerBaseV2(vrfCoordinator)
+    {
         i_entranceFee = _entranceFee;
     }
 
@@ -20,6 +24,16 @@ contract Raffle {
         s_players.push(payable(msg.sender));
         emit RaffleEnter(msg.sender);
     }
+
+    function requestRandomWinner() external {
+        // request a random number
+        // once we get it, do something with it
+    }
+
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
+        internal
+        override
+    {}
 
     function getEntranceFee() public view returns (uint256) {
         return i_entranceFee;
