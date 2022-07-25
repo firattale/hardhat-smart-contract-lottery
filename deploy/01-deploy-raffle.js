9ec85cb3670772a4c9f7348fa8cad6dfdf5040d2
Firat Tale <firat.tale@auto1.com>
2022-07-25 14:22:44 +0200

    create deploy scripts

---
+++ deploy/01-deploy-raffle.js
@@ -0,0 +1,18 @@
+const { network, ethers } = require("hardhat");
+const { developmentChains } = require("../helper-hardhat-config");
+
+module.exports = async function ({ getNamedAccounts, deployments }) {
+	const { deploy, log } = deployments;
+	const { deployer } = await getNamedAccounts();
+	let vrfCoordinatorV2Address;
+	if (developmentChains.includes(network.name)) {
+		const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock");
+		vrfCoordinatorV2Address = vrfCoordinatorV2Mock.address;
+	}
+	const raffle = await deploy("Raffle", {
+		from: deployer,
+		args: [],
+		log: true,
+		waitConfirmations: network.config.blockConfirmations || 1,
+	});
+};
